ee7506855cf434931ae78e0c0841961870adbcc9
AnChuoisss <165070493+AnChuoisss@users.noreply.github.com>
2024-11-27 16:47:01 +0700

    sua dau ""

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Views/Store";
 import Products from "./tinh nang/Products";
@@ -12,26 +7,18 @@ import AddProduct from "./tinh nang/AddProduct";
 import EditProduct from "./tinh nang/EditProduct";
 import "./App.css";
 
-const MainContent = () => {
-  return (
-    <div className="app-container">
-      <div className="main-content">
-        <Routes>
-          <Route path="*" element={<Navigate to="/products" />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/add-product" element={<AddProduct />} />
-          <Route path="/edit-product/:id" element={<EditProduct />} />
-        </Routes>
-      </div>
-    </div>
-  );
-};
-
 const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <MainContent />
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<Navigate to="/products" replace />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/add-product" element={<AddProduct />} />
+            <Route path="/edit-product/:id" element={<EditProduct />} />
+          </Routes>
+        </div>
       </Router>
     </Provider>
   );
